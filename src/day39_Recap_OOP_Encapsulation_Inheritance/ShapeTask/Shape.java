7b69e6b39a280e2983864fa7d20cbad6e4b057eb
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-14 11:26:18 -0500

    day39_Recap

--- src/day39_Recap_OOP_Encapsulation_Inheritance/ShapeTask/Shape.java
+++ src/day39_Recap_OOP_Encapsulation_Inheritance/ShapeTask/Shape.java
@@ -2,7 +2,43 @@ package day39_Recap_OOP_Encapsulation_Inheritance.ShapeTask;
 
 public class Shape {
 
-}
+    private  String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        setName(name);
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+    }
+
+
+
+
 /*
 variable: name
 
--- src/day39_Recap_OOP_Encapsulation_Inheritance/ShapeTask/Square.java
+++ src/day39_Recap_OOP_Encapsulation_Inheritance/ShapeTask/Square.java
@@ -1,6 +1,39 @@
 package day39_Recap_OOP_Encapsulation_Inheritance.ShapeTask;
 
-public class Square {
+public class Square extends Shape {
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side < 0){
+            return;
+        }
+        this.side = side;
+    }
+
+    public Square(double side) {
+        super("Square");
+    }
+
+    public double area() {
+        return side * side;
+    }
+
+    public double perimeter() {
+        return side * 4;
+    }
+
+
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                super.toString()+
+                '}';
+    }
 }
 /*
 square extends Shape
