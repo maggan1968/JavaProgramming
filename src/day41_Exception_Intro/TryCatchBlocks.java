ce543aad8080ca927dfcbab050fe84f0a0bf1cda
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-21 14:18:00 -0500

    day41_AccessModifiers

---
+++ src/day41_Exception_Intro/TryCatchBlocks.java
@@ -0,0 +1,71 @@
+package day41_Exception_Intro;
+
+public class TryCatchBlocks {
+
+    public static void main(String[] args) {
+        System.out.println("Test started");
+
+        try{
+            System.out.println(9/0);
+
+        }catch(ArithmeticException e){
+
+            System.out.println("Catch Block");
+
+            System.out.println("Arithmetic Exception was occurred");
+        }
+
+        System.out.println("Test Completed");
+
+
+        System.out.println("------------------------------------------");
+
+        System.out.println("Test2 started");
+
+        int[] numbers = {1,2,3,4,5,};
+
+        try {
+
+            System.out.println(numbers[200]);
+
+            System.out.println("Try Block");
+
+       // } catch(NullPointerException e) {
+        } catch(RuntimeException e) {
+
+            e.printStackTrace();
+
+          //  System.out.println("Catch Block");
+
+          //  System.out.println("Null Pointer exception was occurred");
+
+           // System.out.println(e.getMessage());
+        }
+
+        System.out.println("Test2 Completed");
+
+        System.out.println("---------------------------------------");
+
+        System.out.println("Test3 started");
+
+
+         try {
+             System.out.println("Cydeo".substring(2, 0));
+         }catch (RuntimeException e){
+              e.printStackTrace();
+         }
+        System.out.println("Test3 Completed");
+
+        System.out.println("-------------------------------");
+
+
+
+
+
+
+
+
+
+
+    }
+}
