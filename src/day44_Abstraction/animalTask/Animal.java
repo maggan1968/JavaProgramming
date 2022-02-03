53f6b2e3b0aebdc0a234defa4c77c2da47ecf80e
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-02-03 11:00:50 -0600

    day44 Abstraction - Interface

--- src/day44_Abstraction/animalTask/Animal.java
+++ src/day44_Abstraction/animalTask/Animal.java
@@ -1,75 +1,105 @@
 package day44_Abstraction.animalTask;
+/*
+class Test{
 
-public  class Animal {
+    public static void main(String[] args) {
 
-        private String name;
-        private final String breed;
-        private final char gender;
-        private int age;
-        private String size;
-        private final String color;
-
-        public final static boolean canBreathe;
-
-        static{
-            canBreathe = true;
-        }
-
-        public Animal(String name, String breed, char gender, int age, String size, String color) {
-            setName(name);
-            this.breed = breed;
-            if ( !(gender == 'M' || gender =='F')){
-                throw new RuntimeException("Invalid gender: "+gender);
-            }
-            this.gender = gender;
-            setAge(age);
-            setSize(size);
-            this.color = color;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            if(name.isEmpty()){
-                throw new RuntimeException("Invalid Name");
-            }
-            this.name = name;
-        }
-
-        public String getBreed() {
-            return breed;
-        }
-
-        public char getGender() {
-            return gender;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public String getSize() {
-            return size;
-        }
-
-        public void setSize(String size) {
-            this.size = size;
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-
-
+        System.out.println(new Test().getClass().getSimpleName());
+        System.out.println(new Test().getClass().getName());
 
     }
+
+}
+ */
+
+public abstract class Animal {
+
+    private String name;
+    private final String breed;
+    private final char gender;
+    private int age;
+    private String size;
+    private final String color;
+
+    public final static boolean canBreathe;
+
+    static {
+        canBreathe = true;
+    }
+
+    public Animal(String name, String breed, char gender, int age, String size, String color) {
+        setName(name);
+        this.breed = breed;
+        if (!(gender == 'M' || gender == 'F')) {
+            throw new RuntimeException("Invalid gender: " + gender);
+        }
+        this.gender = gender;
+        setAge(age);
+        setSize(size);
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            throw new RuntimeException("Invalid Name");
+        }
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public final void drink() {
+        System.out.println(name + "is drinking");//implementation is for all subclasses
+    }
+
+    public abstract void eat();// this method we can add if is abstract, not implementation
+    // other subclasses can have different implementations
+    // final and abstract we can't have together
+
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+"{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
+
 
 
 /*
