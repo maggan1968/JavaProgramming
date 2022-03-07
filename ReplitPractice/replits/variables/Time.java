25ba7b5591cc59ecf83b2f1973882ee9a31645f9
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-03-07 10:00:26 -0600

    Changes

---
+++ ReplitPractice/replits/variables/Time.java
@@ -0,0 +1,28 @@
+package replits.variables;
+
+public class Time {
+    public static void main(String[] args) {
+int hour = 12;
+int minute = 24;
+int second = 33;
+String amOrPm = "PM";
+
+       // Scanner sc = new Scanner(System.in);
+        //hour   = sc.nextInt();
+        //minute = sc.nextInt();
+        //second = sc.nextInt();
+        //amOrPm = sc.next();
+
+        System.out.println(hour + ":" + minute + ":" +second + " " +amOrPm);
+
+    }
+}
+/*
+In this assignment, you will write code to put together time of day.
+Like this: 12:24:33 PM
+
+1. Declare variables: hour, minute, second that can hold int values.
+   Declare amOrPm variable that can hold a String ("AM" or "PM")
+2. Instructor will set different values to your variables
+3. Using the variables and concatenation, print values in the format mentioned above.
+ */
